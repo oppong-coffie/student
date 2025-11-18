976fb19cdc3bf1e729ad7655a7c73eb1362464db
unknown <oppongcoffie27@gmail.com>
2025-11-18 07:32:23 +0000

    settings to Settings

--- src/pages/dashboarb3/Dashboard3.jsx
+++ src/pages/dashboarb3/Dashboard3.jsx
@@ -8,7 +8,7 @@ import PartnerMgnt from "./PartnerMgnt.jsx";
 import Financial from "./Financial.jsx";
 import Transfer from "../Transfer.jsx";
 import Report from "../Report.jsx";
-import Settings from "../settings.jsx";
+import Settings from "../Settings.jsx";
 
 export default function Dashboard() {
   return (
